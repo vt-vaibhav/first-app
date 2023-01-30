9eb49ed101995671c47bef1a469e0f6640efbb5d
vt-vaibhav <vaibhav.savaliya@volansys.com>
2023-01-30 17:27:57 +0530

    changes

---
+++ build/precache-manifest.6ef49db0d73919320d11daad0776f302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213abc48b4b2d074fee255e4e22b1c98",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "4bcfdcfbb9a7b3947e21",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "4bcfdcfbb9a7b3947e21",
+    "url": "/bmi-calculator/static/js/main.64fa15ad.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
